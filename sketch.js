a2130150789585b97be713b04e5e01700ec5d656
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-25 22:31:39 +0200

    some changes

--- sketch.js
+++ sketch.js
@@ -1,44 +1,104 @@
-// Image to ASCII
+const canvas = document.getElementById('preview');
+const fileInput = document.querySelector('input[type="file"');
+const asciiImage = document.getElementById('ascii');
 
-const density = "Ñ@#W$9876543210?!abc;:+=-,._";
-let gloria;
+const context = canvas.getContext('2d');
 
-function preload() {
-  gloria = loadImage("./pic64x64.png");
-}
+const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
 
-function setup() {
-  createCanvas(400, 400);
-}
+const getFontRatio = () => {
+    const pre = document.createElement('pre');
+    pre.style.display = 'inline';
+    pre.textContent = ' ';
 
-function draw() {
-  background(0);
+    document.body.appendChild(pre);
+    const { width, height } = pre.getBoundingClientRect();
+    document.body.removeChild(pre);
 
-  let w = width / gloria.width;
-  let h = height / gloria.height;
-  gloria.loadPixels();
-  for (let i = 0; i < gloria.width; i++) {
-    for (let j = 0; j < gloria.height; j++) {
-      const pixelIndex = (i + j * gloria.width) * 4;
-      const r = gloria.pixels[pixelIndex + 0];
-      const g = gloria.pixels[pixelIndex + 1];
-      const b = gloria.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 3;
+    return height / width;
+};
 
-    //   const c = density.charAt(charIndex);
-    //   if (c == " ") row += "&nbsp;";
-    //   else row += c;
+const fontRatio = getFontRatio();
 
-      noStroke();
-      fill(255);
-      //square(i * w, j * h, w);
+const convertToGrayScales = (context, width, height) => {
+    const imageData = context.getImageData(0, 0, width, height);
 
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
+    const grayScales = [];
 
-      textSize(w);
-      textAlign(CENTER, CENTER);
-      text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
+    for (let i = 0 ; i < imageData.data.length ; i += 4) {
+        const r = imageData.data[i];
+        const g = imageData.data[i + 1];
+        const b = imageData.data[i + 2];
+
+        const grayScale = toGrayScale(r, g, b);
+        imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+
+        grayScales.push(grayScale);
     }
-  }
-}
+
+    context.putImageData(imageData, 0, 0);
+
+    return grayScales;
+};
+
+const MAXIMUM_WIDTH = 80;
+const MAXIMUM_HEIGHT = 80;
+
+const clampDimensions = (width, height) => {
+    const rectifiedWidth = Math.floor(getFontRatio() * width);
+
+    if (height > MAXIMUM_HEIGHT) {
+        const reducedWidth = Math.floor(rectifiedWidth * MAXIMUM_HEIGHT / height);
+        return [reducedWidth, MAXIMUM_HEIGHT];
+    }
+
+    if (width > MAXIMUM_WIDTH) {
+        const reducedHeight = Math.floor(height * MAXIMUM_WIDTH / rectifiedWidth);
+        return [MAXIMUM_WIDTH, reducedHeight];
+    }
+
+    return [rectifiedWidth, height];
+};
+
+fileInput.onchange = (e) => {
+    const file = e.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+        const image = new Image();
+        image.onload = () => {
+            const [width, height] = clampDimensions(image.width, image.height);
+
+            canvas.width = width;
+            canvas.height = height;
+
+            context.drawImage(image, 0, 0, width, height);
+            const grayScales = convertToGrayScales(context, width, height);
+
+            fileInput.style.display = 'none';
+            drawAscii(grayScales, width);
+        }
+
+        image.src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+};
+
+const grayRamp = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+const rampLength = grayRamp.length;
+
+const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
+
+const drawAscii = (grayScales, width) => {
+    const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
+        let nextChars = getCharacterForGrayScale(grayScale);
+        if ((index + 1) % width === 0) {
+            nextChars += '\n';
+        }
+
+        return asciiImage + nextChars;
+    }, '');
+
+    asciiImage.textContent = ascii;
+};
