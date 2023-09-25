c51fd3a0e0b4f7695d8df2818546abda63551794
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-26 01:39:53 +0200

    completed

--- sketch.js
+++ sketch.js
@@ -1,104 +1,109 @@
-const canvas = document.getElementById('preview');
+const canvas = document.getElementById("preview");
 const fileInput = document.querySelector('input[type="file"');
-const asciiImage = document.getElementById('ascii');
+const asciiImage = document.getElementById("ascii");
 
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 
 const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
 
 const getFontRatio = () => {
-    const pre = document.createElement('pre');
-    pre.style.display = 'inline';
-    pre.textContent = ' ';
+  const pre = document.createElement("pre");
+  pre.style.display = "inline";
+  pre.textContent = " ";
 
-    document.body.appendChild(pre);
-    const { width, height } = pre.getBoundingClientRect();
-    document.body.removeChild(pre);
+  document.body.appendChild(pre);
+  const { width, height } = pre.getBoundingClientRect();
+  document.body.removeChild(pre);
 
-    return height / width;
+  return height / width;
 };
 
 const fontRatio = getFontRatio();
 
 const convertToGrayScales = (context, width, height) => {
-    const imageData = context.getImageData(0, 0, width, height);
+  const imageData = context.getImageData(0, 0, width, height);
 
-    const grayScales = [];
+  const grayScales = [];
 
-    for (let i = 0 ; i < imageData.data.length ; i += 4) {
-        const r = imageData.data[i];
-        const g = imageData.data[i + 1];
-        const b = imageData.data[i + 2];
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const r = imageData.data[i];
+    const g = imageData.data[i + 1];
+    const b = imageData.data[i + 2];
 
-        const grayScale = toGrayScale(r, g, b);
-        imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+    const grayScale = toGrayScale(r, g, b);
+    imageData.data[i] =
+      imageData.data[i + 1] =
+      imageData.data[i + 2] =
+        grayScale;
 
-        grayScales.push(grayScale);
-    }
+    grayScales.push(grayScale);
+  }
 
-    context.putImageData(imageData, 0, 0);
+  context.putImageData(imageData, 0, 0);
 
-    return grayScales;
+  return grayScales;
 };
 
 const MAXIMUM_WIDTH = 80;
 const MAXIMUM_HEIGHT = 80;
 
 const clampDimensions = (width, height) => {
-    const rectifiedWidth = Math.floor(getFontRatio() * width);
+  const rectifiedWidth = Math.floor(getFontRatio() * width);
 
-    if (height > MAXIMUM_HEIGHT) {
-        const reducedWidth = Math.floor(rectifiedWidth * MAXIMUM_HEIGHT / height);
-        return [reducedWidth, MAXIMUM_HEIGHT];
-    }
+  if (height > MAXIMUM_HEIGHT) {
+    const reducedWidth = Math.floor((rectifiedWidth * MAXIMUM_HEIGHT) / height);
+    return [reducedWidth, MAXIMUM_HEIGHT];
+  }
 
-    if (width > MAXIMUM_WIDTH) {
-        const reducedHeight = Math.floor(height * MAXIMUM_WIDTH / rectifiedWidth);
-        return [MAXIMUM_WIDTH, reducedHeight];
-    }
+  if (width > MAXIMUM_WIDTH) {
+    const reducedHeight = Math.floor((height * MAXIMUM_WIDTH) / rectifiedWidth);
+    return [MAXIMUM_WIDTH, reducedHeight];
+  }
 
-    return [rectifiedWidth, height];
+  // return [rectifiedWidth, height];
+  return [reducedWidth, reducedHeight];
 };
 
 fileInput.onchange = (e) => {
-    const file = e.target.files[0];
+  const file = e.target.files[0];
 
-    const reader = new FileReader();
-    reader.onload = (event) => {
-        const image = new Image();
-        image.onload = () => {
-            const [width, height] = clampDimensions(image.width, image.height);
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    const image = new Image();
+    image.onload = () => {
+      const [width, height] = clampDimensions(image.width, image.height);
 
-            canvas.width = width;
-            canvas.height = height;
+      canvas.width = width;
+      canvas.height = height;
 
-            context.drawImage(image, 0, 0, width, height);
-            const grayScales = convertToGrayScales(context, width, height);
+      context.drawImage(image, 0, 0, width, height);
+      const grayScales = convertToGrayScales(context, width, height);
 
-            fileInput.style.display = 'none';
-            drawAscii(grayScales, width);
-        }
-
-        image.src = event.target.result;
+      fileInput.style.visibility = "hidden";
+      drawAscii(grayScales, width);
     };
 
-    reader.readAsDataURL(file);
+    image.src = event.target.result;
+  };
+
+  reader.readAsDataURL(file);
 };
 
-const grayRamp = 'Ñ@#W$9876543210?!abc;:+=-,.* ';
+const grayRamp = "Ñ@#W$9876543210?!abc;:+=-,.* ";
 const rampLength = grayRamp.length;
 
-const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
+const getCharacterForGrayScale = (grayScale) =>
+  grayRamp[Math.ceil(((rampLength - 1) * grayScale) / 255)];
 
 const drawAscii = (grayScales, width) => {
-    const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
-        let nextChars = getCharacterForGrayScale(grayScale);
-        if ((index + 1) % width === 0) {
-            nextChars += '\n';
-        }
+  const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
+    let nextChars = getCharacterForGrayScale(grayScale);
+    if ((index + 1) % width === 0) {
+      nextChars += "\n";
+    }
 
-        return asciiImage + nextChars;
-    }, '');
+    return asciiImage + nextChars;
+  }, "");
 
-    asciiImage.textContent = ascii;
+  asciiImage.textContent = ascii;
 };
